72434eed61ed188ed905a8be3581a97fd6c0a29f
emre aydemir <aydemiremre98@gmail.com>
2022-06-09 02:25:20 +0300

    springboot initial setup is done

---
+++ src/main/java/com/aydemir/mappyplaces/MappyPlacesApplication.java
@@ -0,0 +1,13 @@
+package com.aydemir.mappyplaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappyPlacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MappyPlacesApplication.class, args);
+	}
+
+}
